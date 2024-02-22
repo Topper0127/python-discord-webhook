a86fb6216c04764fe67db43be0e3837763736a17
Topper0127 <155635728+Topper0127@users.noreply.github.com>
2024-02-22 15:08:56 -0500

    Update basic_webhook.py

--- examples/basic_webhook.py
+++ examples/basic_webhook.py
@@ -1,4 +1,4 @@
 from discord_webhook import DiscordWebhook
 
-webhook = DiscordWebhook(url="https://discord.com/api/webhooks/1210308309102501989/i-gYUMErI0VHVi5jlV2fMczwGkawiuV2CKG9hr3obUC-m4MFPUlGesQlqvBELxdb-ITZ", content="First msg")
+webhook = DiscordWebhook(url="your url", content="First msg")
 response = webhook.execute()
